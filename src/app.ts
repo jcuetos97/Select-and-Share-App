67d517bd16cdfd1f73f536ff89484e4c75614c01
jcuetos97 <105081149+jcuetos97@users.noreply.github.com>
2022-12-31 12:40:16 -0600

    Added axios GET method for Google Geocoding Response data

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,33 @@
-// Code goes here!
+import axios from "axios";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+let GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+};
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      const coordinates = response.data.results[0].geometry.location;
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+form.addEventListener("submit", searchAddressHandler);
